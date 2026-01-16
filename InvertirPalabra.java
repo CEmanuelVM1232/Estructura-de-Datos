6f883883514b9d8bd5a37bec1f367badf2ac15ce
Carlos Emanuel Valentino Martínez <1224100580.cevm@gmail.com>
2026-01-16 14:49:41 -0600

    Remove commented license and clean up code

--- InvertirPalabra.java
+++ InvertirPalabra.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Ejercicio4;
 
 /**
@@ -82,4 +78,5 @@ public class InvertirPalabra  {
         sc.close();
     }
 }
+
 
