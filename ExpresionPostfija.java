6e907168d0681ef6b1e7a730f79cc4c1ca0fdd9f
Carlos Emanuel Valentino Martínez <1224100580.cevm@gmail.com>
2026-01-16 14:44:43 -0600

    Remove commented license and improve formatting

--- ExpresionPostfija.java
+++ ExpresionPostfija.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Ejercicio8;
 
 /**
@@ -150,3 +146,4 @@ public class ExpresionPostfija {
     }
 
 }
+
