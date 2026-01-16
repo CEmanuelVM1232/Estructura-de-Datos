1d1987af3d33409877f30f66cf17b26ff10a2abc
Carlos Emanuel Valentino Martínez <1224100580.cevm@gmail.com>
2026-01-16 15:41:15 -0600

    Remove commented license and clean up code

--- VerificarPilaVacia.java
+++ VerificarPilaVacia.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Ejercicio3;
 
 /**
  * Demostración de verificación de estado de pila vacía
@@ -98,4 +93,5 @@ public class VerificarPilaVacia {
         sc.close();
     }
 }
+
 
